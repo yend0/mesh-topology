88384d221e16c780f459ac0a2673dce37140d992
Vadim Remezov <aspowersan@protonmail.com>
2024-07-08 11:50:37 +0300

    feature: add functions for logger

---
+++ mesh/headers/logger.h
@@ -0,0 +1,15 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "stdafx.h"
+#include "common.h"
+
+#define LOG_FILE "log_program.log"
+
+extern pthread_mutex_t log_mutex;
+
+const char *get_message_type_string(const message_type type);
+void log_message(const char *creator, const message_type type,
+                 const char *message_format, ...);
+
+#endif // LOGGER_H
