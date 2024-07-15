a69171ef85ab89407497f34d84d7036e2e6d2f6a
Vadim Remezov <aspowersan@protonmail.com>
2024-07-15 11:46:08 +0300

    refactor: change server

--- mesh/sources/server.c
+++ mesh/sources/server.c
@@ -1,13 +1,10 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-#include "graph.h"
-#include "constants.h"
-#include "logger.h"
-#include "packet.h"
+#include "user_interface.h"
 
 pid_t node_pids[MAX_NODES];
-int client_socket;
+int server_socket;
 struct sockaddr_in server_address;
 
 void start_node(const int node_id)
@@ -38,6 +35,7 @@ void stop_node(const int node_id)
         kill(node_pids[node_id], SIGTERM);
         waitpid(node_pids[node_id], NULL, 0);
         node_pids[node_id] = 0;
+        log_message("SERVER", MSG_TYPE_INFO, "Node %d stopped", node_id);
     }
     else
     {
@@ -54,41 +52,43 @@ void handle_signal(const int sig)
             stop_node(i);
         }
     }
-    close(client_socket);
+    close(server_socket);
     exit(EXIT_SUCCESS);
 }
 
-void send_command_to_node(packet_t *packet)
+void handle_user_commands(int graph[MAX_NODES][MAX_NODES], int client_socket)
 {
-    if (packet->mac_packet.ttl == 0)
+    while (1)
     {
-        log_message("SERVER", MSG_TYPE_NOT_VALID_DATA, "TTL expired, packet dropped");
-        return;
+        char command[256];
+        printf("Enter command: ");
+        fgets(command, sizeof(command), stdin);
+
+        int src_node, dest_node, node_id;
+        char message[MAX_MESSAGE_LENGTH];
+
+        if (sscanf(command, "send %d %d %[^\n]", &src_node, &dest_node, message) == 3)
+        {
+            create_and_send_message(src_node, dest_node, graph, MAX_NODES, message, client_socket);
+        }
+        else if (sscanf(command, "broadcast %d %[^\n]", &src_node, message) == 2)
+        {
+            create_and_send_broadcast(src_node, graph, MAX_NODES, message, client_socket);
+        }
+        else if (sscanf(command, "stop %d", &node_id) == 1)
+        {
+            stop_node(node_id);
+            remove_node(node_id, graph);
+        }
+        else if (strncmp(command, "help", 4) == 0)
+        {
+            print_help();
+        }
+        else
+        {
+            printf("Invalid command format. Type 'help' for a list of commands.\n");
+        }
     }
-
-    packet->mac_packet.ttl--;
-
-    struct sockaddr_in node_address;
-    node_address.sin_family = AF_INET;
-    node_address.sin_port = htons(CLIENT_BASE_PORT + packet->mac_packet.mac_sender);
-    node_address.sin_addr.s_addr = INADDR_ANY;
-
-    int sent_bytes = sendto(client_socket, packet, sizeof(packet_t), 0, (struct sockaddr *)&node_address, sizeof(node_address));
-
-    if (sent_bytes == -1)
-    {
-        log_message("SERVER", MSG_TYPE_ERROR, "sendto() failed");
-    }
-}
-
-void create_and_send_command(const int src, const int dest, int graph[MAX_NODES][MAX_NODES],
-                             const int size_graph, const char *message, const int message_id)
-{
-    packet_t packet = create_packet(src, dest, TTL_LIMIT, src, dest, message_id, message);
-
-    memcpy(packet.network_graph, graph, size_graph * size_graph * sizeof(int));
-
-    send_command_to_node(&packet);
 }
 
 int main()
@@ -101,13 +101,11 @@ int main()
     int distances[MAX_NODES];
     int predecessors[MAX_NODES];
 
-    int message_id = 0;
-
     initialize_graph(MAX_NODES, graph);
     add_edges(matrix_size, graph);
 
-    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (client_socket == -1)
+    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_socket == -1)
     {
         log_message("SERVER", MSG_TYPE_ERROR, "Socket creation failed");
         exit(EXIT_FAILURE);
@@ -122,29 +120,7 @@ int main()
         start_node(i);
     }
 
-    while (1)
-    {
-        char command[256];
-        printf("Enter command (send <source_node> <dest_node> <message> | stop <node_id>): ");
-        fgets(command, sizeof(command), stdin);
-
-        int src_node, dest_node, node_id;
-        char message[MAX_MESSAGE_LENGTH];
-        if (sscanf(command, "send %d %d %[^\n]", &src_node, &dest_node, message) == 3)
-        {
-            create_and_send_command(src_node, dest_node, graph, MAX_NODES, message, message_id);
-            message_id++;
-        }
-        else if (sscanf(command, "stop %d", &node_id) == 1)
-        {
-            stop_node(node_id);
-            remove_node(node_id, graph);
-        }
-        else
-        {
-            printf("Invalid command format.\n");
-        }
-    }
+    handle_user_commands(graph, server_socket);
 
     handle_signal(SIGINT);
     return EXIT_SUCCESS;
